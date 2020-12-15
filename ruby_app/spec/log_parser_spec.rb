8e4b631ddb74a82eb0c52b6928c13f2458fbcd93
Tony To <tonyto85@gmail.com>
2020-12-15 15:37:54 +0000

    LogParser parse_unique with single values

--- ruby_app/spec/log_parser_spec.rb
+++ ruby_app/spec/log_parser_spec.rb
@@ -35,11 +35,11 @@ class LogParser
 
   def format_unique_output(key, value)
     unique_value = value.uniq
-    unique_value.length > 1 ? "#{key} #{unique_value.length} unique views\n" : "#{key} #{unique_value.length} unique view"
+    unique_value.length > 1 ? "#{key} #{unique_value.length} unique views\n" : "#{key} #{unique_value.length} unique view\n"
   end
 
   def format_output(key, value)
-    value.length > 1 ? "#{key} #{value.length} visits\n" : "#{key} #{value.length} visit"
+    value.length > 1 ? "#{key} #{value.length} visits\n" : "#{key} #{value.length} visit\n"
   end
 end
 
@@ -48,8 +48,8 @@ RSpec.describe LogParser do
   let(:file_path) { '/path_to_file.log' }
 
   describe '#parse_unique' do
-    context 'all unique visits' do
-      it 'outputs unique views' do
+    context 'multiple unique visits' do
+      it 'outputs unique visit' do
         allow(File).to receive(:foreach)
           .and_yield('/help_page/1 646.865.545.408')
           .and_yield('/help_page/1 646.865.545.407')
@@ -63,6 +63,21 @@ RSpec.describe LogParser do
           "/help_page/1 3 unique views\n" \
           "/career 2 unique views\n" \
           "/about 2 unique views\n"
+        )
+      end
+    end
+
+    context 'single unique visit' do
+      it 'outputs unique visit' do
+        allow(File).to receive(:foreach)
+          .and_yield('/help_page/1 646.865.545.408')
+          .and_yield('/career 646.865.555.444')
+          .and_yield('/about 646.646.775.909')
+
+        expect(subject.parse_unique).to eq(
+          "/help_page/1 1 unique view\n" \
+          "/career 1 unique view\n" \
+          "/about 1 unique view\n"
         )
       end
     end
@@ -91,7 +106,7 @@ RSpec.describe LogParser do
     it 'reads single line' do
       allow(File).to receive(:foreach).and_yield('/help_page/1 646.865.545.408')
 
-      expect(subject.parse).to eq('/help_page/1 1 visit')
+      expect(subject.parse).to eq("/help_page/1 1 visit\n")
     end
 
     it 'reads multiple lines' do
@@ -101,9 +116,9 @@ RSpec.describe LogParser do
         .and_yield('/about 646.646.775.909')
 
       expect(subject.parse).to eq(
-        '/help_page/1 1 visit' \
-        '/career 1 visit' \
-        '/about 1 visit'
+        "/help_page/1 1 visit\n" \
+        "/career 1 visit\n" \
+        "/about 1 visit\n"
       )
     end
   end
