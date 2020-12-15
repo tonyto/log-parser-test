9fa86acc2c36c5a2846f876c287dd7971b77d9c9
Tony To <tonyto85@gmail.com>
2020-12-15 14:49:48 +0000

    LogParser reads single line

--- ruby_app/spec/log_parser_spec.rb
+++ ruby_app/spec/log_parser_spec.rb
@@ -1,13 +1,32 @@
 class LogParser
-  def initialize; end;
+  def initialize(file_path)
+    @file_path = file_path
+  end
 
   def parse
+    result = Hash.new
+    output = ''
 
+    File.foreach(@file_path) do |line|
+      url, ip = line.split(' ')
+      result[url] ? result[url] << ip : result[url] = [ip]
+    end
+
+    result.each{|k, v| output += "#{k} #{v.length} visit"}
+
+    output
   end
 end
 
 RSpec.describe LogParser do
-  describe 'new' do
-    it { expect(described_class.new).to_not be(nil) }
+  subject { LogParser.new(file_path) }
+  let(:file_path) { '/path_to_file.log' }
+
+  describe '#parse' do
+    it 'reads first line' do
+      allow(File).to receive(:foreach).and_yield('/help_page/1 646.865.545.408')
+
+      expect(subject.parse).to eq('/help_page/1 1 visit')
+    end
   end
 end
