7b0e06c5587cbbf815eb6603a1b3df9bb224d8f1
Tony To <tonyto85@gmail.com>
2020-12-15 15:11:02 +0000

    LogParser pluralises outputs

--- ruby_app/spec/log_parser_spec.rb
+++ ruby_app/spec/log_parser_spec.rb
@@ -12,9 +12,15 @@ class LogParser
       result[url] ? result[url] << ip : result[url] = [ip]
     end
 
-    result.each{|k, v| output += "#{k} #{v.length} visit"}
+    result.each{|k, v| output += format_output(k, v)}
 
     output
+  end
+
+  private
+
+  def format_output(key, value)
+    value.length > 1 ? "#{key} #{value.length} visits\n" : "#{key} #{value.length} visit"
   end
 end
 
@@ -23,10 +29,42 @@ RSpec.describe LogParser do
   let(:file_path) { '/path_to_file.log' }
 
   describe '#parse' do
-    it 'reads first line' do
+    context 'multiple visits for a url' do
+      it 'pluralises visits' do
+        allow(File).to receive(:foreach)
+          .and_yield('/help_page/1 646.865.545.408')
+          .and_yield('/help_page/1 646.865.545.408')
+          .and_yield('/help_page/1 646.865.545.408')
+          .and_yield('/career 646.865.555.444')
+          .and_yield('/career 646.865.555.444')
+          .and_yield('/about 646.646.775.909')
+          .and_yield('/about 646.646.775.909')
+
+        expect(subject.parse).to eq(
+          "/help_page/1 3 visits\n" \
+          "/career 2 visits\n" \
+          "/about 2 visits\n"
+        )
+      end
+    end
+
+    it 'reads single line' do
       allow(File).to receive(:foreach).and_yield('/help_page/1 646.865.545.408')
 
       expect(subject.parse).to eq('/help_page/1 1 visit')
+    end
+
+    it 'reads multiple lines' do
+      allow(File).to receive(:foreach)
+        .and_yield('/help_page/1 646.865.545.408')
+        .and_yield('/career 646.865.555.444')
+        .and_yield('/about 646.646.775.909')
+
+      expect(subject.parse).to eq(
+        '/help_page/1 1 visit' \
+        '/career 1 visit' \
+        '/about 1 visit'
+      )
     end
   end
 end
